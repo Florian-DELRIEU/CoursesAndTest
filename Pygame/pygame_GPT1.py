080cc534fdae3e07851031c740ebcf769bdddc9c
Florian DELRIEU <florian.delrieu@free.fr>
2024-05-20 23:17:11 +0200

    merge in blobs
    - proie et predateur regroupé en blobs
    - action move ajouté

--- Pygame/pygame_GPT1.py
+++ Pygame/pygame_GPT1.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import numpy as np
 
 # Initialisation de Pygame
 pygame.init()
@@ -17,29 +18,26 @@ RED = (255, 0, 0)
 
 
 # Classes pour les proies et les prédateurs
-class Proie:
+class Blob:
     def __init__(self):
         self.x = random.randint(0, width)
         self.y = random.randint(0, height)
-        self.size = 5
-
-    def draw(self, window):
-        pygame.draw.rect(window, GREEN, (self.x, self.y, self.size, self.size))
-
-
-class Predateur:
-    def __init__(self):
-        self.x = random.randint(0, width)
-        self.y = random.randint(0, height)
+        self.direction = random.randint(0,360)
+        self.speed = 1
         self.size = 10
 
     def draw(self, window):
         pygame.draw.rect(window, RED, (self.x, self.y, self.size, self.size))
 
+    def move(self,model="drunk_man"):
+        if model == "drunk_man":
+            self.direction = random.randint(0,360)
+            self.x += self.speed * np.cos(self.direction)
+            self.y += self.speed * np.sin(self.direction)
+
 
 # Liste de proies et de prédateurs
-proies = [Proie() for _ in range(50)]
-predateurs = [Predateur() for _ in range(10)]
+Blobs = [Blob() for _ in range(10)]
 
 # Boucle principale
 running = True
@@ -47,15 +45,13 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
     # Dessiner la fenêtre
     window.fill(WHITE)
+    for blob in Blobs:
+        blob.move()
+        blob.draw(window)
 
     # Dessiner les proies et les prédateurs
-    for proie in proies:
-        proie.draw(window)
-    for predateur in predateurs:
-        predateur.draw(window)
 
     pygame.display.update()
 
